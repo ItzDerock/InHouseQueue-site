b753b242873d965e751bda7603e279ca7f1f3349
Kristler <kristler@dmginc.gg>
2023-06-17 16:02:25 +0100

    Adding automatic updating of stats cards

--- src/partials/Home/StatCards.tsx
+++ src/partials/Home/StatCards.tsx
@@ -1,23 +1,59 @@
+"use client";
+
+import React from "react";
+
 import StatCard from "../../components/StatCard/StatCard";
 
+interface IStatsProps {
+  status: string;
+  last_check: string;
+  message: string;
+  server_count: number;
+  total_games: number;
+  total_users: number;
+}
+
 export default function StatCards() {
+  const [stats, setStats] = React.useState<IStatsProps | undefined>();
+
+  React.useEffect(() => {
+    const header = {
+      "method": "GET",
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+      "mode": "no-cors"
+    };
+
+    const fetchStats = async () => {
+      const data: IStatsProps = await fetch("https://health.inhousequeue.xyz/health", { headers: header })
+        .then((res) => {
+          return res.json()
+        })
+        .catch((err) => console.log(err))
+
+      setStats(data)
+    };
+
+    fetchStats();
+  }, [])
+
   return (
     <div className="relative w-full z-30">
       <div className="flex -translate-y-24 flex-row flex-wrap z-30 w-full align-middle justify-center gap-9">
         <StatCard
-          count={600}
+          count={stats?.server_count ?? 0}
           label="Servers"
           aosIndex={0}
         />
 
         <StatCard
-          count={8000}
-          label="Users"
+          count={stats?.total_users ?? 0}
+          label="Active Players"
           aosIndex={1}
         />
 
         <StatCard
-          count={80000}
+          count={stats?.total_games ?? 0}
           label="Matches Played"
           aosIndex={2}
         />
