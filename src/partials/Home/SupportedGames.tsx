10d5d446ed5f606604f0719d533b00455885dfbe
Henry <Henry_Koleoso@hotmail.co.uk>
2023-10-26 22:49:48 +0100

    feature: remove br

--- src/partials/Home/SupportedGames.tsx
+++ src/partials/Home/SupportedGames.tsx
@@ -10,7 +10,6 @@ export default function SupportedGames() {
       <div className="h-full">
         <h1 className="font-bold text-3xl text-white">Draft, Duel, Dominate</h1>
         <p className="text-gray-400">Designed for competitive 5v5 games such as:</p>
-        <br>
         <p className="text-gray-400">Dynamic support for games from 1v1 to 8v8:</p>
       </div>
 
