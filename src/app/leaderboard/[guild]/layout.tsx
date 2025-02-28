7b3e896e61092c53df72713a21770ba2b56c40f1
Derock <derock@derock.dev>
2025-02-28 11:20:34 -0500

    feat: leaderboard auth bar (#141)

--- src/app/leaderboard/[guild]/layout.tsx
+++ src/app/leaderboard/[guild]/layout.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import AuthHeader from "./_components/AuthHeader";
 
 export default function LeaderboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <AuthHeader />
+      {/* The auth header needs to push everything down,
+          so wrap in an relative container to "reset"
+          the origin for future absolute containers */}
+      <div className="relative">{children}</div>
+    </SessionProvider>
+  );
 }
